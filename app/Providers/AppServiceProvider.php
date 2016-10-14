e40910d21daed394e9b2fa02ab34c61ca9b014c8
Duy Luu <duykhluu1989@gmail.com>
2016-10-14 11:57:49 +0700

    Tinymce composer manage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__ . '/../../vendor/tinymce/tinymce/skins' => base_path('assets/js/skins'),
+            __DIR__ . '/../../vendor/tinymce/tinymce/tinymce.min.js' => base_path('assets/js/tinymce.min.js'),
+        ], 'tinymce');
     }
 
     /**
