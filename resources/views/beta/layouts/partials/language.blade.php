a1472ba06683b9a1e2cf11a57b79f709d820bbb8
Duy Luu <duykhluu1989@gmail.com>
2016-12-15 15:49:22 +0700

    Change language text to image

--- resources/views/beta/layouts/partials/language.blade.php
+++ resources/views/beta/layouts/partials/language.blade.php
@@ -1,4 +1,8 @@
 <div class="language">
-    <a href="{{ url('lang', ['lan' => 'en']) }}"<?php echo (App::getLocale() == 'en' ? ' class="active"' : ''); ?>>EN</a>
-    <a href="{{ url('lang', ['lan' => 'vi']) }}"<?php echo (App::getLocale() == 'vi' ? ' class="active"' : ''); ?>>VI</a>
+    <a href="{{ url('lang', ['lan' => 'en']) }}"<?php echo (App::getLocale() == 'en' ? ' class="active"' : ''); ?>>
+        <img src="{{ asset('assets/img/ico_en.png') }}" width="80%" />
+    </a>
+    <a href="{{ url('lang', ['lan' => 'vi']) }}"<?php echo (App::getLocale() == 'vi' ? ' class="active"' : ''); ?>>
+        <img src="{{ asset('assets/img/ico_vn.png') }}" width="80%" />
+    </a>
 </div>
