0c0c98a2f8c2e13a7770e6b436a0d74efa7d8d26
Duy Luu <duykhluu1989@gmail.com>
2017-01-12 11:43:37 +0700

    Change thankyou fb script

--- resources/views/beta/pages/thank_you.blade.php
+++ resources/views/beta/pages/thank_you.blade.php
@@ -68,7 +68,7 @@
             t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
                 document,'script','https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '1665387320368867');
-        fbq('track', "PageView");
+        fbq('track', "Registration");
     </script>
     <noscript>
         <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1665387320368867&ev=PageView&noscript=1" />
