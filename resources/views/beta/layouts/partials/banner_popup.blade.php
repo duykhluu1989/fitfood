502191ab13a777de0390e01ee5b027eec7f4480f
Duy Luu <duykhluu1989@gmail.com>
2017-03-29 13:30:46 +0700

    Change banner popup with url width

--- resources/views/beta/layouts/partials/banner_popup.blade.php
+++ resources/views/beta/layouts/partials/banner_popup.blade.php
@@ -15,10 +15,10 @@
 
             $.magnificPopup.open({
                 items: {
-                    src: '<a style="position: relative;width: auto;max-width: 70%;margin: 40px auto;display: block" href="<?php echo $banner['url']; ?>" target="_blank"><img src="<?php echo $banner['image_src']; ?>" width="100%" alt="Fitfood" border="0" /></a>'
+                    src: '<a style="position: relative;width: auto;max-width: 60%;margin: 40px auto;display: block" href="<?php echo $banner['url']; ?>" target="_blank"><img src="<?php echo $banner['image_src']; ?>" width="100%" alt="Fitfood" border="0" /></a>'
                 },
                 closeBtnInside: false,
-                closeMarkup: '<button style="margin-right: 15%" title="Close (Esc)" type="button" class="mfp-close">&#215;</button>',
+                closeMarkup: '<button style="margin-right: 20%" title="Close (Esc)" type="button" class="mfp-close">&#215;</button>',
                 type: 'inline'
             });
 
