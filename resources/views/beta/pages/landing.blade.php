a91431d06c8ac28037ec41d96c7c5f2a17787969
Duy Luu <duykhluu1989@gmail.com>
2018-03-20 10:09:22 +0700

    Add script

--- resources/views/beta/pages/landing.blade.php
+++ resources/views/beta/pages/landing.blade.php
@@ -56,6 +56,20 @@
     ga('require', 'displayfeatures');
     ga('send', 'pageview');
 </script>
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 940209987;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/940209987/?guid=ON&amp;script=0"/>
+    </div>
+</noscript>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-75679054-1"></script>
 <script>
